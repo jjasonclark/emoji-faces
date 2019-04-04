6c3cc63806c1f8ae0830c6e18985cbbd5a8eb4c4
Jason Clark <jason@jjasonclark.com>
2019-04-03 19:19:35 -0700

    Updates

---
+++ precache-manifest.99f7248ad78f3ceceffa4e65d4cff92e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2547ec50378eb6da959",
+    "url": "/emoji-faces/static/js/runtime~main.96fdb10a.js"
+  },
+  {
+    "revision": "eed4d03721a2926236cf",
+    "url": "/emoji-faces/static/js/main.632499f9.chunk.js"
+  },
+  {
+    "revision": "7f58e854116f13b9c517",
+    "url": "/emoji-faces/static/js/2.9c0f4d9f.chunk.js"
+  },
+  {
+    "revision": "eed4d03721a2926236cf",
+    "url": "/emoji-faces/static/css/main.c7994335.chunk.css"
+  },
+  {
+    "revision": "5b4984df2e7a1e98bab54e955d0612eb",
+    "url": "/emoji-faces/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/emoji-faces/precache-manifest.99f7248ad78f3ceceffa4e65d4cff92e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/emoji-faces/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
